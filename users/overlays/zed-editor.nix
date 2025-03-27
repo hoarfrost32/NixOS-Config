eb65b1d8a8628bc81b1edd67199147bfc58c0f9b
hoarfrost32 <aditya.tejpaul@research.iiit.ac.in>
2025-03-27 19:44:21 +0530

    zed-editor nightly overlay

--- users/overlays/zed-editor.nix
+++ users/overlays/zed-editor.nix
@@ -1,12 +1,22 @@
 final: prev: {
   zed-editor = prev.zed-editor.overrideAttrs (oldAttrs: rec {
-    version = "nightly";
+    version = "0.180.0-pre";
     src = prev.fetchFromGitHub {
       owner = "zed-industries";
       repo = "zed";
-      tag = "${version}";
-      sha256 = "sha256-r0mcIGmBuGp3wDuHIZrb9tQ/vZ74DvAfLMuJFJNlyfY=";
+      tag = "v${version}";
+      sha256 = "sha256-RSINcf3pDNyvBBlFOTvrITUk8SxPqn7foq2qUSrZQD0=";
     };
+    
+    # cargoHash = "";
+    
+    patches = [ ./patches/generate-licenses.patch ];
+    
+    postPatch = ''
+      substituteInPlace $cargoDepsCopy/webrtc-sys-*/build.rs \
+        --replace-fail "cargo:rustc-link-lib=static=webrtc" "cargo:rustc-link-lib=dylib=webrtc"
+    '';
+    
     # buildInputs = (oldAttrs.buildInputs or []) ++ [ prev.libnotify ];
   });
 }
