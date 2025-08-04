5c1a5e734a1d4dfc21e42d864bc4d08bd69ffa8b
hoarfrost32 <aditya.tejpaul@research.iiit.ac.in>
2025-08-05 00:46:07 +0530

    Overlays

--- users/overlays/default.nix
+++ users/overlays/default.nix
@@ -2,7 +2,6 @@
 
 final: prev:
   let
-    # protonVpnOverlay = import ./proton-vpn.nix final prev;
     zedEditorOverlay = import ./zed-editor.nix final prev zeditor-input;
     tyOverlay = import ./ty.nix final prev zeditor-input;
     llamaOverlay = import ./llama-cpp.nix final prev; 
